cd68b727707d9f219bd8732cbc827ed5113f143b
clayton easterbrooks & kendra ash <student@epicodus.com>
2015-10-13 11:28:18 -0700

    add form to add a new post

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,4 +4,23 @@ class LinksController < ApplicationController
     @links = Link.all
   end
 
+  def new
+    @link = Link.new
+    render :new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to links_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
 end
--- spec/features/view_link_pages_spec.rb
+++ spec/features/view_link_pages_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 describe "path to homepage" do
   it "lists all Links" do
-    link_test = Link.create(title: "Test link 3", url: "www.yahoo.com")
+    link_test = Link.create(title: "Test link 3", url: "http://www.yahoo.com")
     visit "links"
     expect(page).to have_content(link_test.title)
   end
+  # 
+  # it 'visits link' do
+  #   link_test = Link.create(title: "Test link 3", url: "http://www.google.com")
+  #   visit 'links'
+  #   # save_and_open_page
+  #   click_link link_test.title
+  #   expect(page).to have_content('Google Search')
+  #
+  # end
 end
